75a915b7dc9349fc099c2d8c49b9e56064f8b2ca
rileymcdowell <riley.mcdowell.0@gmail.com>
2018-10-07 17:11:05 +0000

    working secure docker registry

--- docker-registry/down.sh
+++ docker-registry/down.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 kubectl delete -Rf ./converted
+kubectl delete secrets docker-registry-cert
 rm -rf ./converted/*.yaml
 rm -rf ./converted/*.yaml.bak
+
--- docker-registry/up.sh
+++ docker-registry/up.sh
@@ -10,6 +10,10 @@ pushd converted
 # Modifications here, if needed.
 python3 modify_claims.py
 python3 modify_service.py
+python3 modify_deployment.py
 popd
+
+# Create the certificate kubernetes "generic" secret to mount as files
+kubectl create secret generic docker-registry-cert --from-file=./certs/docker.lan.crt --from-file=./certs/docker.lan.key
 
 kubectl apply -Rf converted
