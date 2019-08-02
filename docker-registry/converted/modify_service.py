5481a00c07e682753639ba3889904c8346ef3fdb
rileymcdowell <riley.mcdowell.0@gmail.com>
2019-08-02 18:42:38 -0500

    Add ui to docker registry

--- docker-registry/converted/modify_service.py
+++ docker-registry/converted/modify_service.py
@@ -1,13 +1,7 @@
 #!/usr/bin/env python
 
 """
-The purpose of this script is to configure separate udp and tcp services 
-because while metallb supports this, kubernetes does not allow 
-one load balancer to balance both udp and tcp traffic.
-
-This service also sets the IP addresses of various deployed services.
-
-https://github.com/kubernetes/kubernetes/pull/64471
+This script sets the IP addresses of various deployed services.
 
 """
 
@@ -19,29 +13,22 @@ import codecs
 with open('../../deployment-config.yaml') as f:
     config = yaml.load(f)
     
-ip_address = config['docker-registry']['ip-address']
-external_traffic_policy = config['docker-registry']['external-traffic-policy']
+for service_name in ('docker-registry', 'docker-registry-ui'):
+    ip_address = config[service_name]['ip-address']
+    external_traffic_policy = config[service_name]['external-traffic-policy']
 
-if os.path.exists('./docker-registry-service.yaml'):
-    os.rename('./docker-registry-service.yaml', './docker-registry-service.yaml.bak')
+    if os.path.exists('./{}-service.yaml'.format(service_name)):
+        os.rename('./{}-service.yaml'.format(service_name), './{}-service.yaml.bak'.format(service_name))
 
-with open('./docker-registry-service.yaml.bak') as f:
-    data = yaml.load(f)
+    with open('./{}-service.yaml.bak'.format(service_name)) as f:
+        data = yaml.load(f)
 
-# Write out a version with TCP services only.
-tcp_data = copy.deepcopy(data)
-keep_ports = []
-for port in tcp_data['spec']['ports']:
-    if 'protocol' in port and port['protocol'] == 'UDP':
-        continue
-    else:
-        keep_ports.append(port)
-tcp_data['spec']['ports'] = keep_ports
-tcp_data['metadata']['name'] += '-tcp'
-tcp_data['metadata']['annotations']['metallb.universe.tf/allow-shared-ip'] = 'docker-registry'
-tcp_data['spec']['loadBalancerIP'] = ip_address
-tcp_data['spec']['externalTrafficPolicy'] = external_traffic_policy 
-with open('./docker-registry-service-tcp.yaml', 'w', encoding='utf-8') as f:
-    yaml.dump(tcp_data, f)
+    # Write out a version with TCP services only.
+    tcp_data = copy.deepcopy(data)
+    tcp_data['metadata']['annotations']['metallb.universe.tf/allow-shared-ip'] = service_name
+    tcp_data['spec']['loadBalancerIP'] = ip_address
+    tcp_data['spec']['externalTrafficPolicy'] = external_traffic_policy
+    with open('./{}-service.yaml'.format(service_name), 'w', encoding='utf-8') as f:
+        yaml.dump(tcp_data, f)
 
 
